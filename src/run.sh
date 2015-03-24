19fcc1133775e1b4c65919722f66dbb1a1aa221d
finallyjustice <dongli.zhang0129@gmail.com>
2015-03-24 10:27:10 -0400

    chaneg Makefile and run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,6 +1,5 @@
 echo run armv7 without qemu.log
-qemu-system-arm -M vexpress-a15 -cpu cortex-a15 -m 128  -nographic \
--singlestep -kernel kernel.elf 
+qemu-system-arm -M vexpress-a15 -cpu cortex-a15 -m 128 -kernel kernel.elf -serial stdio
 # skip: -singlestep
 # try skip -cpu, as str r0, [fp,#-8] not write onto mem
 # -cpu cortex-a15
